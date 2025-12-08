b096fdb4979dd24bbed096919abf0f5af7424df7
Dr Mohammad <mohaamaad7@gmail.com>
2025-12-08 23:17:45 +0200

    fix: Use Filament v4 Schemas namespace for Grid and Section
    Filament v4 moved Grid and Section from Forms\Components to Schemas\Components

--- app/Filament/Resources/EmailTemplates/Schemas/EmailTemplateForm.php
+++ app/Filament/Resources/EmailTemplates/Schemas/EmailTemplateForm.php
@@ -4,13 +4,13 @@ namespace App\Filament\Resources\EmailTemplates\Schemas;
 
 use App\Models\EmailTemplate;
 use Filament\Forms\Components\ColorPicker;
-use Filament\Forms\Components\Grid;
-use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TagsInput;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Components\Grid;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 use Illuminate\Support\Str;
 
