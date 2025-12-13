49daf1a00081ce7192a2e6c6fa6e84a99b93690e
Dr Mohammad <mohaamaad7@gmail.com>
2025-12-13 16:29:32 +0200

    fix: remove deprecated settings.blade.php causing view:cache error

--- resources/views/filament/pages/settings.blade.php
+++
@@ -1,7 +0,0 @@
-<x-filament-panels::page>
-    <x-filament-panels::form wire:submit="save">
-        {{ $this->form }}
-
-        <x-filament::actions :actions="$this->getFormActions()" />
-    </x-filament-panels::form>
-</x-filament-panels::page>
