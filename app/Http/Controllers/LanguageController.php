0d975d2e047d9cb9a2b4569af3afcc0981a6d9e4
Dr Mohammad <mohaamaad7@gmail.com>
2025-12-14 23:21:26 +0200

    fix change language middleware

--- app/Http/Controllers/LanguageController.php
+++ app/Http/Controllers/LanguageController.php
@@ -12,10 +12,21 @@ class LanguageController extends Controller
         if (!in_array($locale, ['ar', 'en'])) {
             abort(404);
         }
-        
+
         // Set locale in session
         session(['locale' => $locale]);
-        
+
+        // Update user preference if logged in (for any guard)
+        foreach (['web', 'customer'] as $guard) {
+            if (auth($guard)->check()) {
+                $user = auth($guard)->user();
+                if ($user) {
+                    $user->locale = $locale;
+                    $user->save();
+                }
+            }
+        }
+
         // Redirect back
         return redirect()->back();
     }
