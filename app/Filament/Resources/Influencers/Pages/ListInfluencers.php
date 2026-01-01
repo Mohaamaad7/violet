979d6e9893b563d108c5613cb2594347c9e84d8f
Dr Mohammad <mohaamaad7@gmail.com>
2026-01-01 19:34:56 +0200

    fix add influencers

--- app/Filament/Resources/Influencers/Pages/ListInfluencers.php
+++ app/Filament/Resources/Influencers/Pages/ListInfluencers.php
@@ -3,9 +3,19 @@
 namespace App\Filament\Resources\Influencers\Pages;
 
 use App\Filament\Resources\Influencers\InfluencerResource;
+use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
 class ListInfluencers extends ListRecords
 {
     protected static string $resource = InfluencerResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            CreateAction::make()
+                ->label(trans_db('admin.action.create')),
+        ];
+    }
 }
+
